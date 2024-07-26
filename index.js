e5bc9fd05bc804dbc630e7db86801e6c10efa2b1
Nulck <130108938+limboBens@users.noreply.github.com>
2024-07-26 08:59:06 -0300

    index.js

--- index.js
+++ index.js
@@ -24,7 +24,7 @@ function createBot () {
 const bot = mineflayer.createBot({
   host: 'farosas.aternos.me', 
   version: false, // U can replace with 1.16.5 for example, remember to use ', = '1.16.5'
-  username: 'Bot', 
+  username: 'SG_Bot', 
   port: 18946, 
   plugins: [AutoAuth],
   AutoAuth: 'bot112022'
